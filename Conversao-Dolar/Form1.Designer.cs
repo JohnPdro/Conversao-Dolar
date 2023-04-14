db186e3894f995a13f2bf16472e2bfb7239cb860
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-04-14 19:41:36 -0300

    Adicionar arquivos de projeto.

---
+++ Conversao-Dolar/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Conversao_Dolar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDolar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxReal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDolar
+            // 
+            this.textBoxDolar.Location = new System.Drawing.Point(23, 32);
+            this.textBoxDolar.Multiline = true;
+            this.textBoxDolar.Name = "textBoxDolar";
+            this.textBoxDolar.Size = new System.Drawing.Size(250, 24);
+            this.textBoxDolar.TabIndex = 0;
+            this.textBoxDolar.TextChanged += new System.EventHandler(this.textBoxDolar_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VALOR EM DOLAR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(20, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "VALOR EM REAL";
+            // 
+            // textBoxReal
+            // 
+            this.textBoxReal.Location = new System.Drawing.Point(23, 97);
+            this.textBoxReal.Multiline = true;
+            this.textBoxReal.Name = "textBoxReal";
+            this.textBoxReal.Size = new System.Drawing.Size(250, 24);
+            this.textBoxReal.TabIndex = 2;
+            this.textBoxReal.TextChanged += new System.EventHandler(this.textBoxReal_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(23, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CONVERTER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Khaki;
+            this.btnLimpar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnLimpar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkKhaki;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Location = new System.Drawing.Point(183, 142);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(90, 35);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(298, 200);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxReal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDolar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDolar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxReal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Conversao-Dolar/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversao_Dolar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxReal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (textBoxDolar.Text == "")
+            {
+                MessageBox.Show("Por favor insira um valor!", "Aviso!", MessageBoxButtons.OK);
+                textBoxDolar.Focus();
+                return;
+            }
+
+            textBoxReal.Text = (Convert.ToDouble(textBoxDolar.Text) * 4.94).ToString();
+
+
+        }
+
+        private void textBoxDolar_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            textBoxReal.Clear();
+            textBoxDolar.Clear();
+        }
+    }
+}
